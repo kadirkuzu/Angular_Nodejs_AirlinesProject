c28d59503c4494d4d538315795e03c717ba876cb
kadirkuzu <kadirkuzu.5656@gmail.com>
2022-12-12 16:23:53 +0300

    WIP

--- AirlinesProject/src/app/components/shared/table/table.component.ts
+++ AirlinesProject/src/app/components/shared/table/table.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class TableComponent implements OnInit {
   @Input() list:any = []
   @Input() columns:any = []
+  @Input() tableName!:string
   constructor() { }
 
   ngOnInit(): void {
